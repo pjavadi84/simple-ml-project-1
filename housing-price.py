41fc4be7a558c0b2cbdae39b1600723353bcdeab
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-03-10 22:14:41 -0700

    training a model by linear regression

--- housing-price.py
+++ housing-price.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 # Load dataset
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
@@ -21,3 +23,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print(X_test)
 
 
+# Training the model by linear regression:unbiased estimate of how well your model is likely to perform on data it has never seen before.
+# Initialize the model
+model = LinearRegression()
+
+# Fit the model
+model.fit(X_train, y_train)
+
+print(model)
