995a8cffe87f5abab74587bc1514e6129f153264
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2024-03-10 21:37:50 -0700

    loading dataset

--- housing-price.py
+++ housing-price.py
@@ -1,2 +1,7 @@
 from sklearn.datasets import load_boston
 import pandas as pd
+
+# Load dataset
+boston = load_boston()
+data = pd.DataFrame(boston.data, columns=boston.feature_names)
+data['MEDV'] = boston.target
